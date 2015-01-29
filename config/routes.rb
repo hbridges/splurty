7db2de2c8a3106b07c6e2a3a8e8151a058cc8459
Harvey Bridges <hdbridges11@gmail.com>
2015-01-29 04:00:14 +0000

    Created about page and setup link to it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Splurty::Application.routes.draw do
+
+   root 'quotes#index'
+
+   resources :quotes
+   get 'about', :to => 'quotes#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,9 +59,7 @@ Splurty::Application.routes.draw do
   #     resources :products
   #   end
 
-  root 'quotes#index'
 
-  resources :quotes
 
 
 end
